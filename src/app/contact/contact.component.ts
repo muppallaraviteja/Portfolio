142dc7106bb55557baa56c5c3e5c7474bb9001a3
Ravi <ravshanth_2003@tamu.edu>
2019-04-25 14:53:10 -0500

    Portfolio V2
    PORTFOLIO V2

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
+  data: any = [];
+  constructor( private http: HttpClient) { }
 
   ngOnInit() {
+  }
+
+  save(name, email, subject, message): void {
+    this.data['name'] = name;
+    this.data["email"] = email;
+    this.data["subject"] = subject;
+    this.data["message"] = message;
+    console.log(this.data);
+    this.http.put<any>('http://localhost/api/v1/update/', this.data).subscribe(
+      res => {
+        console.log(res);
+      },
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          console.log('Client-side error occured.');
+        } else {
+          console.log('Server-side error occurred.');
+        }
+      });
   }
 
 }
